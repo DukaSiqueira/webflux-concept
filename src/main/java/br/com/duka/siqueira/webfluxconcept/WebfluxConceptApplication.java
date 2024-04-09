e43efc38707db73793ffd377e7f0ac18a82eedcf
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-04-09 08:38:11 -0300

    Start Project

---
+++ src/main/java/br/com/duka/siqueira/webfluxconcept/WebfluxConceptApplication.java
@@ -0,0 +1,13 @@
+package br.com.duka.siqueira.webfluxconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/duka/siqueira/webfluxconcept/WebfluxConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duka.siqueira.webfluxconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
