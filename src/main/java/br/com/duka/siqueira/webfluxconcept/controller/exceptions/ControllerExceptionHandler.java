19a064ae958c9315b7803aa4a46b8708395e3d83
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-04-09 14:15:04 -0300

    Add exception handler validation error

--- src/main/java/br/com/duka/siqueira/webfluxconcept/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/duka/siqueira/webfluxconcept/controller/exceptions/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package br.com.duka.siqueira.webfluxconcept.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -29,6 +31,25 @@ public class ControllerExceptionHandler {
                         )
                 );
 
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException ex, ServerHttpRequest request
+    ) {
+        ValidationError error = new ValidationError(
+                now(),
+                request.getURI().getPath(),
+                BAD_REQUEST.value(),
+                "Validation error",
+                "Error on validation attributes"
+        );
+
+        for (FieldError x :  ex.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
     }
 
     private String veifyDupKey(String message) {
--- src/main/java/br/com/duka/siqueira/webfluxconcept/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/duka/siqueira/webfluxconcept/controller/impl/UserControllerImpl.java
@@ -4,6 +4,7 @@ import br.com.duka.siqueira.webfluxconcept.controller.UserController;
 import br.com.duka.siqueira.webfluxconcept.model.request.UserRequest;
 import br.com.duka.siqueira.webfluxconcept.model.response.UserResponse;
 import br.com.duka.siqueira.webfluxconcept.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class UserControllerImpl implements UserController {
     private final UserService service;
 
     @Override
-    public ResponseEntity<Mono<Void>> save(final UserRequest request) {
+    public ResponseEntity<Mono<Void>> save(@Valid final UserRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.save(request).then());
     }
