872b77c8780e76fc7a21786ff7c3d714984aeecf
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-04-09 17:30:08 -0300

    Add string validator

--- src/main/java/br/com/duka/siqueira/webfluxconcept/model/request/UserRequest.java
+++ src/main/java/br/com/duka/siqueira/webfluxconcept/model/request/UserRequest.java
@@ -1,18 +1,23 @@
 package br.com.duka.siqueira.webfluxconcept.model.request;
 
 
+import br.com.duka.siqueira.webfluxconcept.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
-
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
+
+        @TrimString
         @Email(message = "invalid e-mail")
         @NotBlank(message = "must not be null or empty")
         String email,
+
+        @TrimString
         @Size(min = 8, max = 20, message = "must be between 8 and 20 characters")
         @NotBlank(message = "must not be null or empty")
         String password
