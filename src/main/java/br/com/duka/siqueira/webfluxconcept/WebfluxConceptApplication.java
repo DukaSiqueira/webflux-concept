0dafab3b6f284b220be7331b7e73f2b37cd4d04f
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-04-15 11:21:18 -0300

    adapt

--- src/main/java/br/com/duka/siqueira/webfluxconcept/WebfluxConceptApplication.java
+++ src/main/java/br/com/duka/siqueira/webfluxconcept/WebfluxConceptApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebfluxConceptApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxConceptApplication.class, args);
 	}
-
 }
